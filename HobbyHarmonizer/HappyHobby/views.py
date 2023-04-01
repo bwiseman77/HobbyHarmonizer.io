79e1d1a1d904d69e7bce7c0eafde08f6cf463f85
Christine Van Kirk <cvankir2@nd.edu>
2023-04-01 14:07:15 -0400

    update

--- HobbyHarmonizer/HappyHobby/views.py
+++ HobbyHarmonizer/HappyHobby/views.py
@@ -26,19 +26,5 @@ def login_view(request):
             
     return render(request, 'login.html/', context={'form': form, 'message' : message})
 
-def login_action(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        # Redirect to a success page.
-        print('login success')
-        return HttpResponse("login success!")
-    else:
-        # Return an 'invalid login' error message.
-        print('invalid login')
-        return HttpResponse("invalid login silly!")
-
 def logout_view(request):
     logout(request)
