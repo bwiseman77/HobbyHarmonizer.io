4d0721e8858c71428eb0f1056dd31c5da3918623
Christine Van Kirk <cvankir2@nd.edu>
2023-04-01 13:20:34 -0400

    chagnes

--- HobbyHarmonizer/HobbyHarmonizer/settings.py
+++ HobbyHarmonizer/HobbyHarmonizer/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'HobbyHarmonizer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
