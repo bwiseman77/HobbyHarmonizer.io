4a1393044314638e31fdadc0c32c6bf06e0444a3
Mia Manabat <miamanabat@gmail.com>
2023-04-02 01:32:54 -0400

    start of profile page

--- HobbyHarmonizer/HappyHobby/views.py
+++ HobbyHarmonizer/HappyHobby/views.py
@@ -6,10 +6,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . import forms
 from HappyHobby.forms import SignUpForm
-from django.views.generic import CreateView
-from .models import Image
+from django.views.generic import CreateView, ListView
+from .models import Image, Event
 from django.urls import reverse_lazy
 
 def login_view(request):
@@ -45,6 +46,18 @@ def hostedEvents_view(request):
 def detailView_view(request):
     return render(request, 'detailView.html/')
 
+class UserEventListView(LoginRequiredMixin, ListView):
+    model = Event # query the post model to create the list of articles
+    template_name = 'profile.html'
+    context_object_name = 'events' # if I don't set context_object_name here, it will be object_list to iterate through the list of posts in the html page.
+    ordering = ['-creation_date']
+
+    # return the list of items for this view
+    def get_queryset(self):
+        # return Post.objects.filter(author = self.request.user, approved=True) 
+        return Event.objects.filter(author = self.request.user.profile)
+
+
 def logout_view(request):
     print("logging out")
     logout(request)
@@ -61,14 +74,13 @@ def signup(request):
             user.profile.name = form.cleaned_data.get('name')
             user.profile.save()
             user.save()
-            #user = authenticate(username=user.username, password=raw_password)
             login(request, user)
             return redirect('/picture')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
-class CreatePostView(CreateView):  # new
+class CreateImageView(CreateView):  # new
     model = Image
     form_class = forms.PostForm
     template_name = "picture.html"
@@ -76,13 +88,10 @@ class CreatePostView(CreateView):  # new
 
     def form_valid(self, form):
         response = super(CreatePostView, self).form_valid(form)
-        obj = form.save()
-        self.request.user.profile.image = obj
-        self.request.user.save()
-        print(self.request)
-        print(self.request.user)
-        print("hello")
-        self.request.user.profile.save()
+        obj = form.save(commit=False)
+        obj.profile = self.request.user.profile
+        obj.profile.save()
+        obj.save()
         return response
 
-        
+
